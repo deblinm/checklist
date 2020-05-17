6b992b4a283bd101c701602bbe4f50b7bf6a8340
deblinm <53627794+deblinm@users.noreply.github.com>
2020-05-16 22:13:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+   span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
 
 function inputLength() {
 	return input.value.length;
@@ -12,6 +22,19 @@ function createListElement() {
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+
+	var span = document.createElement("SPAN");
+  	var txt = document.createTextNode("\u00D7");
+  		span.className = "close";
+  		span.appendChild(txt);
+  		li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
 function addListAfterClick() {
@@ -40,4 +63,3 @@ button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 
 ul.addEventListener("click",toggleList);
-
